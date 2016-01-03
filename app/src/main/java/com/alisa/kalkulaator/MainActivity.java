3b2787fbb0ce1946e574e5032288d436f685ad41
SmiAlisa <alisa.dudinova@itcollege.ee>
2016-01-03 18:45:16 +0200

    working, not split

--- app/src/main/java/com/alisa/kalkulaator/MainActivity.java
+++ app/src/main/java/com/alisa/kalkulaator/MainActivity.java
@@ -1,12 +1,9 @@
 package com.alisa.kalkulaator;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -27,13 +24,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btnDiv;
     private Button btnEq;
 
-    private String value1 = "";
-    private String value2 = "";
-    private String operation = "";
-    private boolean operationPressed = false;
+    double value = 0;
+    String operation = "";
+    boolean operationPressed = false;
     private Animation animation;
     private boolean picFlag = false;
-    public String result ="";
 
 
     @Override
@@ -45,12 +40,11 @@ public class MainActivity extends AppCompatActivity {
         animation = AnimationUtils.loadAnimation(this, R.anim.button_anim);
 
 
-        if (savedInstanceState != null) {
-            value1 = savedInstanceState.getString("value1");
-            value2 = savedInstanceState.getString("value2");
+        if (savedInstanceState != null){
+            value =savedInstanceState.getDouble("value");
             operation = savedInstanceState.getString("operation");
             operationPressed = savedInstanceState.getBoolean("pressed");
-            picFlag = savedInstanceState.getBoolean("flag");
+            picFlag = savedInstanceState.getBoolean("screenState");
             screen.setSelected(picFlag);
         }
         screen.setText(R.string.dflt);
@@ -61,55 +55,53 @@ public class MainActivity extends AppCompatActivity {
         btnEq = (Button) findViewById(R.id.buttonEq);
 
 
+
+
     }
 
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
+    public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putString("value1", value1);
-        savedInstanceState.putString ("value2", value2);
+        savedInstanceState.putDouble("value", value);
         savedInstanceState.putString("operation", operation);
         savedInstanceState.putBoolean("pressed", operationPressed);
-        savedInstanceState.putBoolean("flag", picFlag);
+        savedInstanceState.putBoolean ("screenState", picFlag);
 
     }
 
-    public void buttonClick(View view) {
+    public void buttonClick (View view) {
         screen.setSelected(true);
         picFlag = true;
-        switch (view.getId()) {
+        switch (view.getId()){
             // any math operation button
             case R.id.buttonSum:
             case R.id.buttonSub:
             case R.id.buttonMlt:
             case R.id.buttonDiv: {
                 view.startAnimation(animation);
-                Button b = (Button) view;
+                Button b = (Button)view;
 
-                if (!value1.equals("")) {
-                    Intent intent=new Intent("com.alisa.calculator.action.calculate");
-                    this.startService(intent);
-                    //////////////////////////////
+                if (value != 0){
                     btnEq.performClick();
-                    ////////////////////////////////////
                     operationPressed = true;
                     operation = b.getText().toString();
                 } else {
                     operationPressed = true;
                     operation = b.getText().toString();
-                    value1 = String.valueOf(screen.getText());
+                    value = getDouble(screen.getText());
                 }
+
 
                 break;
             }
 
 
-            case R.id.buttonC: {
+            case R.id.buttonC:{
                 screen.setSelected(false);
                 picFlag = false;
                 view.startAnimation(animation);
                 screen.setText(R.string.dflt);
-                value1 = "";
+                value = 0;
                 operation = "";
                 operationPressed = false;
 
@@ -117,25 +109,24 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
 
-            case R.id.buttonEq: {
-
+            case R.id.buttonEq:{
                 view.startAnimation(animation);
-                switch (operation) {
+                switch (operation){
 
-                    case "+": {
-                        screen.setText(String.valueOf(value + getDouble(screen.getText())));
+                    case "+":{
+                        screen.setText (String.valueOf(value + getDouble (screen.getText())));
                         break;
                     }
-                    case "-": {
-                        screen.setText(String.valueOf(value - getDouble(screen.getText())));
+                    case "-":{
+                        screen.setText (String.valueOf(value - getDouble (screen.getText())));
                         break;
                     }
-                    case "*": {
-                        screen.setText(String.valueOf(value * getDouble(screen.getText())));
+                    case "*":{
+                        screen.setText (String.valueOf(value * getDouble (screen.getText())));
                         break;
                     }
-                    case "/": {
-                        screen.setText(String.valueOf(value / getDouble(screen.getText())));
+                    case "/":{
+                        screen.setText (String.valueOf(value / getDouble (screen.getText())));
                         break;
                     }
                     default:
@@ -146,17 +137,18 @@ public class MainActivity extends AppCompatActivity {
                 operationPressed = true;
 
 
+
                 break;
             }
 
             // all numeric buttons
-            default: {
+            default:{
                 view.startAnimation(animation);
 
-                Button b = (Button) view;
+                Button b = (Button)view;
 
                 // in case of new input, start from scratch
-                if ((String.valueOf(screen.getText()).equals(getResources().getString(R.string.dflt))) || (String.valueOf(screen.getText()).equals(getResources().getString(R.string.zero))) || (operationPressed == true)) {
+                if ((String.valueOf(screen.getText()).equals(getResources().getString(R.string.dflt))) ||(String.valueOf(screen.getText()).equals(getResources().getString(R.string.zero)))|| (operationPressed == true)) {
                     operationPressed = false;
                     screen.setText("");
                 }
@@ -167,16 +159,21 @@ public class MainActivity extends AppCompatActivity {
         }// end button switch
     }
 
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
 
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Bundle bundle = intent.getExtras();
-            if (bundle != null) {
-                result = bundle.getString("EXTRA_RESULT");
 
-            }
+    private double getDouble(Object screenInput) {
+        double result;
+        try{
+            result = Double.valueOf(screenInput.toString());
+        } catch (Exception e){
+            screen.setText(R.string.error);
+            e. printStackTrace();
+            result = 0;
         }
-    };
-}
+        return result;
 
+
+    }
+
+
+}
